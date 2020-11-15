58247fdca39c383baac3c7720895234aeae25978
JR Mishra <jrmnitdgp@gmail.com>
2020-11-15 20:49:42 +0530

    Used MVC

---
+++ EmpManagementWebApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpManagementWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from MVC Home -> Controller -> Index Action";
+        }
+    }
+}
--- EmpManagementWebApp/Startup.cs
+++ EmpManagementWebApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,7 @@ namespace EmpManagementWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting= false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,10 +44,12 @@ namespace EmpManagementWebApp
 
             app.UseStaticFiles();
 
-            app.Run(async(context) => {
-                throw new Exception("Custom exception");
-                await context.Response.WriteAsync("Hosting Environment : "+env.EnvironmentName);
-            });
+            app.UseMvcWithDefaultRoute();
+
+            //app.Run(async(context) => {
+            //    //throw new Exception("Custom exception");
+            //    await context.Response.WriteAsync("Employee Management Tool");
+            //});
 
         }
     }

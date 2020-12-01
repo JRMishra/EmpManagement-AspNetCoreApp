ff81faae4eeb21af06bf6d96a54f4133993f8233
JR Mishra <jrmnitdgp@gmail.com>
2020-12-02 00:01:32 +0530

    Configure AppDbContext class and associated option

--- EmpManagementWebApp/Startup.cs
+++ EmpManagementWebApp/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using EmpManagementWebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmpManagementWebApp
 {
@@ -26,6 +27,7 @@ namespace EmpManagementWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDbConnection")));
             services.AddMvc(options => options.EnableEndpointRouting= false);
             services.AddSingleton<IEmployeeRepo, MockEmployeeRepo>();
         }

6907f4817eca34e7fef3731de959969554f35b7d
JR Mishra <jrmnitdgp@gmail.com>
2020-12-02 20:25:42 +0530

    Seed data using extension method

--- EmpManagementWebApp/Models/AppDbContext.cs
+++ EmpManagementWebApp/Models/AppDbContext.cs
@@ -16,29 +16,7 @@ namespace EmpManagementWebApp.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().HasData(
-                new Employee
-                {
-                    Id = 1,
-                    Name = "Jyoti Ranjan",
-                    Department = Dept.IT,
-                    Email = "jrm@mymail.com"
-                },
-                new Employee
-                {
-                    Id = 2,
-                    Name = "Supratim Dey",
-                    Department = Dept.Marketing,
-                    Email = "supdey@mymail.com"
-                },
-                new Employee
-                {
-                    Id = 3,
-                    Name = "Sayan Dash",
-                    Department = Dept.Operation,
-                    Email = "sayan@mymail.com"
-                }
-            );
+            modelBuilder.Seed();
         }
     }
 }
---
+++ EmpManagementWebApp/Models/ModelBuilderExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpManagementWebApp.Models
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Jyoti Ranjan",
+                    Department = Dept.IT,
+                    Email = "jrm@mymail.com"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Supratim Dey",
+                    Department = Dept.Marketing,
+                    Email = "supdey@mymail.com"
+                },
+                new Employee
+                {
+                    Id = 3,
+                    Name = "Sayan Dash",
+                    Department = Dept.Operation,
+                    Email = "sayan@mymail.com"
+                }
+            );
+        }
+    }
+}

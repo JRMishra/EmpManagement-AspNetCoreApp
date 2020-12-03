1f25fe1cb6fe077ba61e769b07dfd86f5201d7dc
JR Mishra <jrmnitdgp@gmail.com>
2020-12-03 18:56:49 +0530

    use logger service

--- EmpManagementWebApp/Controllers/ErrorController.cs
+++ EmpManagementWebApp/Controllers/ErrorController.cs
@@ -5,18 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EmpManagementWebApp.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
             switch(statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry... The resource you requested could not be found";
+                    logger.LogWarning($"404 Error Occured. Path = {statusCodeResult.OriginalPath}" +
+                        $"and QueryString = {statusCodeResult.OriginalQueryString}");
                     break;
                 default:
                     break;
@@ -26,13 +37,11 @@ namespace EmpManagementWebApp.Controllers
 
         [Route("Error")]
         [AllowAnonymous]
-        public IActionResult Error(int statusCode)
+        public IActionResult Error()
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            ViewBag.ExceptionPath = exceptionDetails.Path;
-            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
-            ViewBag.StackTrace = exceptionDetails.Error.StackTrace;
-
+            logger.LogError($"The Path {exceptionDetails.Path} threw an exception" +
+                $"{exceptionDetails.Error}");
             return View("Error");
         }
     }
